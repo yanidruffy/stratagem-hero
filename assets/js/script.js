355f7c7401e7c054d412ef32a04b8701f302ca80
yanidruffy <111918613+yanidruffy@users.noreply.github.com>
2024-04-23 13:12:40 +0200

    Add function displayStratagem

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
+let stratagemName = "";
+
 // wait for DOM to load
 document.addEventListener("DOMContentLoaded", function() {
-
 
 	let buttons = document.getElementsByClassName("btn");
 	for (let button of buttons) {
@@ -14,11 +15,21 @@ document.addEventListener("DOMContentLoaded", function() {
 			startGame();
 		}
 	})
-
-	function startGame() {
-		document.getElementById("hidden").innerHTML = "";
-		alert("Game Started!");
-	}
-
 });
 
+function startGame() {
+	document.getElementById("hidden").innerHTML = "";
+	alert("Game Started!");
+
+	let num = 0;
+	// let num = Math.floor(Math.random() * 2);
+
+	displayStratagem(num);
+};
+
+function displayStratagem(num) {
+	if (num === 0) {
+		stratagemName = "500kg Bomb";
+		document.getElementById("stratagem").innerHTML = stratagemName;
+	}
+}
